0e34b8bbd7ebfc849a20af4ba5ec7a3f61b2fa4a
KhushiGoyal08 <khushi2112053@akgec.ac.in>
2024-07-06 20:21:31 +0530

    string

--- Javascript/pr_set_loop_func.js
+++ Javascript/pr_set_loop_func.js
@@ -6,3 +6,17 @@ let marks = {
 for (let i = 0; i < Object.keys(marks).length; i++) {
     console.log("The marks of " + Object.keys(marks)[i] + " are " + marks[Object.keys(marks)[i]]);
 }
+
+// OR this can be done using for in loop
+
+for(let key in marks){
+    console.log("The marks of "+key +" are "+ marks[key]);
+}
+
+// mean of  number
+
+const mean =(a,b,c,d)=>{
+    return (a+b+c+d)/4;
+}
+console.log(mean(4,5,6,7));
+// string complete --- template literal and its methods like concat,trim
